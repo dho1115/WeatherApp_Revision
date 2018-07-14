c2c2b5d8f89aff99328806e00c03d1ae30ff3b88
DAVID HO <davidhorph@hotmail.com>
2018-07-14 02:19:03 -0500

    Added input bar, submit button...
    Also, made the temperature a circle.

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,15 +1,28 @@
 
 
-var EnterLocation = "Chicago, IL";
+let temp = document.querySelector(".temp");
+let circle = document.querySelector(".circle");
+let tempNumber = parseInt(temp.textContent);
+let input = document.querySelector("input");
 
-var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + EnterLocation + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+let defaultLocation = "Chicago,IL";
+
+function NewLocation() {
+    defaultLocation = input;
+    return defaultLocation;
+} 
+
+var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + defaultLocation + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
 $.getJSON
 (url,(jsonData) => 
-{console.log(jsonData.query.results.channel.item.title);
+{$("#place").text(jsonData.query.results.channel.item.title);
 
 console.log("CONDITION(S): ",jsonData.query.results.channel.item.condition);
+
+$("#date").text(jsonData.query.results.channel.item.condition.date);
 $(".temp").text(jsonData.query.results.channel.item.condition.temp);
+tempColor(tempNumber);
 
 console.log("WIND CHILL:  ",jsonData.query.results.channel.wind.chill);
 
@@ -17,6 +30,32 @@ console.log("WINDSPEED: ",jsonData.query.results.channel.wind.speed);
 
 console.log("WIND DIRECTION: ",jsonData.query.results.channel.wind.direction);
 });
+
+function tempColor(num) {
+    
+    if(tempNumber > 105) {
+        circle.style.backgroundColor = "FF0000";
+    } 
+
+    if(tempNumber > 75) {
+        circle.style.backgroundColor = "F54747";
+    } 
+
+    if(tempNumber > 60) {
+        circle.style.backgroundColor = "E18787";
+    } 
+
+    if(tempNumber > 25) {
+        circle.style.backgroundColor = "E18787";
+    } 
+
+    else {
+        circle.style.backgroundColor = "0767B0";
+    }
+    
+    return circle.style.backgroundColor;
+} 
+
 
 
 
