63bcdf652f09a2e20b0d42db51451b266b9fde4f
DAVID HO <davidhorph@hotmail.com>
2018-07-14 21:19:56 -0500

    How I got user input to work...
    To make my WEATHER APP be able to have the capability to accept user input OR return a default value if the user enters a "bad input", I: (1) Created a function that accepts 2 possible arguments ( user value or "default), (2) ENCAPSULATED the URL variable inside that function with a return value of $.getJSON() and (3) called that function.

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,5 +1,4 @@
 
-
 let temp = document.querySelector(".temp");
 let circle = document.querySelector(".circle");
 let tempNumber = parseInt(temp.textContent);
@@ -7,60 +6,35 @@ let input = document.querySelector("input");
 
 var defaultLocation = "Chicago,IL";
 
-function NewLocation() {
-    defaultLocation = input;
-    alert(input.value);
-    console.log(input.value);
-    return defaultLocation;
+
+//Create a FUNCTION that:
+//(1) Can accept one of two arguments and (use the "||") and will use whichever returns TRUE. One will be the variable that the user enters (put this on the left side of the "||") and the other will act as the "default" value if the user enters a "falsy value" or doesn't enter one at all.
+//(2) Encapsulates the "var" that defines the URL.
+//(3) RETURNS the ENTIRE $.getJSON() value.
+
+function FindWeatherConditions(enterlocation = document.querySelector("#enterlocation").value || "Chicago,IL") {
+
+    var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + enterlocation + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+    return $.getJSON
+    (url,(jsonData) => 
+    {$("#place").text(jsonData.query.results.channel.item.title);
+    
+    console.log("CONDITION(S): ",jsonData.query.results.channel.item.condition);
+    $("#weather").text(jsonData.query.results.channel.item.condition.text);
+    $("#date").text(jsonData.query.results.channel.item.condition.date);
+    $(".temp").text(jsonData.query.results.channel.item.condition.temp);
+    
+    console.log("WIND CHILL:  ",jsonData.query.results.channel.wind.chill);
+    $("#windchill").text(jsonData.query.results.channel.wind.chill);
+    
+    console.log("WINDSPEED: ",jsonData.query.results.channel.wind.speed);
+    $("#windspeed").text(jsonData.query.results.channel.wind.speed);
+    
+    console.log("WIND DIRECTION: ",jsonData.query.results.channel.wind.direction);
+    });
 } 
 
-var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + defaultLocation + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+//CALL the function (that was defined above) below this comment.
 
-$.getJSON
-(url,(jsonData) => 
-{$("#place").text(jsonData.query.results.channel.item.title);
-
-console.log("CONDITION(S): ",jsonData.query.results.channel.item.condition);
-$("#weather").text(jsonData.query.results.channel.item.condition.text);
-$("#date").text(jsonData.query.results.channel.item.condition.date);
-$(".temp").text(jsonData.query.results.channel.item.condition.temp);
-
-tempColor(tempNumber);
-
-console.log("WIND CHILL:  ",jsonData.query.results.channel.wind.chill);
-$("#windchill").text(jsonData.query.results.channel.wind.chill);
-
-console.log("WINDSPEED: ",jsonData.query.results.channel.wind.speed);
-$("#windspeed").text(jsonData.query.results.channel.wind.speed);
-
-console.log("WIND DIRECTION: ",jsonData.query.results.channel.wind.direction);
-});
-
-function tempColor(num) {
-    
-    if(tempNumber > 105) {
-        circle.style.backgroundColor = "FF0000";
-    } 
-
-    if(tempNumber > 75) {
-        circle.style.backgroundColor = "F54747";
-    } 
-
-    if(tempNumber > 60) {
-        circle.style.backgroundColor = "E18787";
-    } 
-
-    if(tempNumber > 25) {
-        circle.style.backgroundColor = "E18787";
-    } 
-
-    else {
-        circle.style.backgroundColor = "0767B0";
-    }
-    
-    return circle.style.backgroundColor;
-} 
-
-
-
-
+FindWeatherConditions();
